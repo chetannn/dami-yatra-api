ac1696b708ca0c025261e9d73d5ac16a27040457
chetannn <chetankharel7@gmail.com>
2022-06-26 12:30:31 +0545

    wip

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     /**
-     * @throws AuthenticationException
+     * @throws ValidationException
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request) : JsonResponse
     {
-        if(!auth()->attempt($request->only('email', 'password'))) {
-            throw new AuthenticationException();
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
+
+        return new JsonResponse(data: [
+            'token' => $user->createToken('authToken')->plainTextToken
+        ]);
     }
 }
--- app/Http/Controllers/Vendor/CouponController.php
+++ app/Http/Controllers/Vendor/CouponController.php
@@ -5,10 +5,19 @@ namespace App\Http\Controllers\Vendor;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Symfony\Component\HttpFoundation\Response;
 
 class CouponController extends Controller
 {
+    public function index() : LengthAwarePaginator
+    {
+        return auth()->user()
+            ->vendor()
+            ->first()
+            ->coupons()
+            ->paginate(\request('per_page', 10));
+    }
     public function store(Request $request) : JsonResponse
     {
         $validated = $request->validate([
